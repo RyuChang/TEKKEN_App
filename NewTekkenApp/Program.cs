8af3777922cbba79f71417f46745da414117e6b1
zbkof <zbkof@RC-PC>
2022-02-15 22:02:10 +0900

    구글 로그인 추가

--- NewTekkenApp/Program.cs
+++ NewTekkenApp/Program.cs
@@ -1,8 +1,10 @@
+using System.Runtime.InteropServices;
 using System.Text.Json.Serialization;
 using Blazored.Modal;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using Microsoft.Net.Http.Headers;
 using NewTekkenApp.Areas.Identity;
 using NewTekkenApp.Data;
@@ -13,8 +15,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
 builder.Services.AddDbContext<TekkenDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("tekkenConnection"), b => b.MigrationsAssembly("TekkenApp")).EnableSensitiveDataLogging(), ServiceLifetime.Transient);
@@ -22,17 +26,49 @@ builder.Services.AddControllers().AddJsonOptions(x =>
                     {
                         x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
                         x.JsonSerializerOptions.WriteIndented = true;
-
                     }
                     );
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
+
+
+builder.Services.AddAuthentication().AddGoogle(googleOptions =>
+{
+    var configuration = builder.Configuration;
+    googleOptions.ClientId = configuration["Authentication:Google:ClientId"];
+    googleOptions.ClientSecret = configuration["Authentication:Google:ClientSecret"];
+});
+
+//   .AddFacebook(options =>
+//   {
+//       IConfigurationSection FBAuthNSection =
+//       config.GetSection("Authentication:FB");
+//       options.ClientId = FBAuthNSection["ClientId"];
+//       options.ClientSecret = FBAuthNSection["ClientSecret"];
+//   })
+//   .AddMicrosoftAccount(microsoftOptions =>
+//   {
+//       microsoftOptions.ClientId = config["Authentication:Microsoft:ClientId"];
+//       microsoftOptions.ClientSecret = config["Authentication:Microsoft:ClientSecret"];
+//   })
+//   .AddTwitter(twitterOptions =>
+//   {
+//       twitterOptions.ConsumerKey = config["Authentication:Twitter:ConsumerAPIKey"];
+//       twitterOptions.ConsumerSecret = config["Authentication:Twitter:ConsumerSecret"];
+//       twitterOptions.RetrieveUserDetails = true;
+//   });
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddBlazoredModal();
+
 builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter(); // 개발 환경에 유용한 오류 정보 제공
+
+builder.Services.AddBlazoredModal();
+
 builder.Services.AddSingleton<HttpClient>();
 
 builder.Services.AddSingleton<WeatherForecastService>();
@@ -74,24 +110,24 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllerRoute(
-         name: "API",
-         pattern: "{area:exists}/{controller=Moves}/{action=Index}/{id?}");
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapControllerRoute(
+//         name: "API",
+//         pattern: "{area:exists}/{controller=Moves}/{action=Index}/{id?}");
 
-    endpoints.MapControllerRoute(
-         name: "API",
-         pattern: "{area:exists}/{controller=Commands}/{action=Index}");
-
-
-    endpoints.MapAreaControllerRoute(
-        name: "default",
-        areaName: "Admin",
-        pattern: "{area:exists}/{controller=Move}/{action=Index}/{id?}");
+//    endpoints.MapControllerRoute(
+//         name: "API",
+//         pattern: "{area:exists}/{controller=Commands}/{action=Index}");
 
 
-});
+//    endpoints.MapAreaControllerRoute(
+//        name: "default",
+//        areaName: "Admin",
+//        pattern: "{area:exists}/{controller=Move}/{action=Index}/{id?}");
+
+
+//});
 
 app.UseCors(policy =>
     policy.WithOrigins("https://localhost:7275;", "http://localhost:5275")

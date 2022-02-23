9f84bbb8b33caa2a069ef7c4ebb8e6f9edbe4888
zbkof <zbkof@RC-PC>
2022-02-24 01:56:50 +0900

    커맨드 입력 초기 세팅 수정

--- NewTekkenApp/Pages/Admin/MoveCommands/Edit.razor.cs
+++ NewTekkenApp/Pages/Admin/MoveCommands/Edit.razor.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Blazored.Modal.Services;
+using Blazored.Modal;
+using System.Collections;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.JSInterop;
 using NewTekkenApp.Pages.Admin.Components.Base.Data;
 using TekkenApp.Models;
@@ -31,7 +35,6 @@ namespace NewTekkenApp.Pages.Admin.MoveCommands
         {
             if (firstRender)
             {
-                module = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./moveCommand.js");
                 InitCommand();
             }
             //    //if(module is not null)  await module.InvokeAsync<object>("test2");
@@ -48,6 +51,177 @@ namespace NewTekkenApp.Pages.Admin.MoveCommands
                 _stateGroupCode = int.Parse(stateGroupCode);
                 state = StateService?.GetEntitiesWithNameByStateGroup(int.Parse(stateGroupCode));
                 StateHasChanged();
+            }
+        }
+
+
+        [CascadingParameter] public IModalService Modal { get; set; }
+
+        public async Task ShowMovesModal()
+        {
+            var parameters = new ModalParameters();
+            parameters.Add(nameof(moveEntity.Character_code), moveEntity.Character_code);
+
+            var moveModal = Modal.Show<MoveListComponent>("State Move", parameters);
+            var result = await moveModal.Result;
+
+            if (result.Cancelled)
+            {
+                Console.WriteLine("Modal was cancelled");
+            }
+            else
+            {
+                //await JSRuntime.InvokeVoidAsync("AddMovesModal", result.Data);
+
+                if (module is not null) await module.InvokeAsync<object>("commandUtil.AddMovesModal", result.Data);
+                Console.WriteLine("Modal was closed");
+            }
+        }
+
+        async Task ShowTextModal()
+        {
+
+            //url = "/Admin/MoveText/SelectMoveText";
+
+            //        var result = rawCommand + '/{T:' + stateCode + ':' + $('#move').val() + '}';
+
+        }
+        //    void ShowMovesModal() => Modal.Show<MoveListComponent>("State Move");
+
+        private int timer { get; set; } = 0;
+        public List<string> resultKey { get; set; }
+        public List<string> clickedKey { get; set; }
+        private Hashtable keyMap = new Hashtable();
+        public string rawCommand { get; set; }
+        public string displayCommand { get; set; }
+
+        Boolean keyDown = false;
+
+        public void InitCommand()
+        {
+            SetKeyMap();
+            rawCommand = moveEntity.MoveCommand.Description;
+            
+            clickedKey = new List<string>();
+            resultKey = new List<string>();
+
+        }
+        public void ClearCommand()
+        {
+            clickedKey.Clear();
+            timer = 0;
+        }
+
+        public async Task SetKeyDown(KeyboardEventArgs e)
+        {
+            AddKey(e.Key);
+            timer++;
+            Console.WriteLine("SetKeyDown");
+        }
+
+        public async Task SetKeyUp(KeyboardEventArgs e)
+        {
+            RemoveKey(e.Key);
+            if (resultKey.Count == 0)
+            {
+                AddCommand(e.Key);
+            }
+            timer = 0;
+            Console.WriteLine("SetKeyUp");
+
+        }
+
+        public void AddKey(string key)
+        {
+
+            if (!clickedKey.Contains(key))
+            {
+                clickedKey.Add(key);
+                resultKey.Add(key);
+            }
+        }
+
+        public void AddCommand(string key)
+        {
+            string result = rawCommand;
+            string formedKey = "";
+
+            if (key == "Backspace")
+            {
+                int lastIndex = rawCommand.LastIndexOf("/");
+                if (lastIndex < 0) lastIndex = 0;
+                SetCommand(rawCommand.Substring(0, lastIndex));
+                return;
+            }
+            else if (clickedKey.Count == 1)
+            {
+                formedKey = key.ToUpper();
+            }
+            else if (clickedKey.Count >= 2)
+            {
+                clickedKey.Sort();
+
+                formedKey = String.Join("+", clickedKey.ToArray()).ToUpper();
+            }
+
+            if (timer > 2)
+            {
+                formedKey = 'L' + formedKey.ToUpper();
+            }
+
+
+            string mapppedKey = MapKey(formedKey);
+
+
+            if (mapppedKey != "")
+            {
+                result += '/' + mapppedKey;
+            }
+
+            SetCommand(result);
+            clickedKey.Clear();
+        }
+
+
+        public string MapKey(string formedKey)
+        {
+            if (keyMap[formedKey] != null)
+            {
+                return $"[{keyMap[formedKey].ToString().Trim()}]";
+            }
+            return "";
+        }
+
+        public void SetCommand(string result)
+        {
+            if (result.Length > 0 && result[0] == '/')
+            {
+                result = result.Substring(1, result.Length - 1);
+            }
+
+            rawCommand = result;
+            moveEntity.MoveCommand.Description = rawCommand;
+            displayCommand = rawCommand.Replace("/", " ");
+
+            ClearCommand();
+        }
+
+        public void RemoveKey(string key)
+        {
+            bool isExist = this.resultKey.Contains(key);
+            if (isExist)
+            {
+                resultKey.Remove(key);
+            }
+        }
+
+        public async void SetKeyMap()
+        {
+            var results = await httpClient.GetFromJsonAsync<MoveListVM[]>("https://localhost:44354/api/commands");
+
+            foreach (var result in results)
+            {
+                keyMap[result.Key] = result.Code;
             }
         }
     }

365158c42bd271beab694818dbda553c6712a4a6
zbkof <zbkof@RC-PC>
2022-03-01 01:51:13 +0900

    CharacterSelectBox 자료형 변경

--- NewTekkenApp/Pages/Common/Components/CharacterSelectBox.razor.cs
+++ NewTekkenApp/Pages/Common/Components/CharacterSelectBox.razor.cs
@@ -6,21 +6,18 @@ namespace NewTekkenApp.Pages.Common.Components
 {
     public partial class CharacterSelectBox
     {
-        [Inject]
-        protected ICharacterService characterService { get; set; } = default!;
+        [Parameter] public EventCallback<string> OnClickCallback { get; set; }
+        [Parameter] public Action<int>? OnCharacterChanged { get; set; }
+        [Parameter] public int CharacterCode { get; set; }
+
+        [Inject] protected ICharacterService characterService { get; set; } = default!;
 
         public List<SelectListItem> selectListItems { get; set; } = default!;
-
-        [Parameter]
-        public EventCallback<string> OnClickCallback { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
             selectListItems = await characterService.GetSelectItems();
         }
-
-        [Parameter]
-        public Action<String>? OnCharacterChanged { get; set; }
     }
 }
 

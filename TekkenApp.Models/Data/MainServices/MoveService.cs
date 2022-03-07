3e53a2b5af0c38380380ae6e0e6103d55c1130e4
zbkof <zbkof@RC-PC>
2022-03-08 01:24:53 +0900

    사용자 기술 출력 다국어 적용

--- TekkenApp.Models/Data/MainServices/MoveService.cs
+++ TekkenApp.Models/Data/MainServices/MoveService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
@@ -23,10 +24,14 @@ namespace TekkenApp.Data
         {
             return await _dataDbSet.Where(m => m.Id == id).Include(m => m.MoveCommand).ThenInclude(c => c.NameSet).FirstOrDefaultAsync();
         }
-
         public async Task<IEnumerable<Move>> GetMoveListWithCommandsByCharacterCodeAsync(int Character_code)
         {
-            return await _dataDbSet.Where(m => m.Character_code == Character_code).Include(m => m.MoveCommand).ThenInclude(c => c.NameSet).Include(m => m.MoveData).ThenInclude(c => c.NameSet).ToListAsync<Move>();
+            return await _dataDbSet.Where(m => m.Character_code == Character_code)
+                .Include(m => m.MoveCommand)
+                .ThenInclude(c => c.NameSet.Where(n=>n.Language_code.Equals(CultureInfo.CurrentCulture.TwoLetterISOLanguageName)))
+                .Include(m => m.MoveData)
+                .ThenInclude(c => c.NameSet)
+                .ToListAsync<Move>();
         }
     }
 }

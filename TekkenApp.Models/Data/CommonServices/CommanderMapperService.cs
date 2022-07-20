2924d2a9549f1e363294de1e9a640a4311af6496
zbkof <zbkof@RC-PC>
2022-07-20 23:35:21 +0900

    키보드 입력 컴포넌트 추가2

--- TekkenApp.Models/Data/CommonServices/CommanderMapperService.cs
+++ TekkenApp.Models/Data/CommonServices/CommanderMapperService.cs
@@ -7,6 +7,9 @@ using TekkenApp.Models.Models.Maps;
 
 namespace TekkenApp.Data
 {
+    /// <summary>
+    /// 키와 코드를 입력 받아서, 시스템에 있는 값으로 매핑해주는  클래스
+    /// </summary>
     public class CommanderMapperService : ICommanderMapperService
     {
         Dictionary<string, string> KeyMap { get; set; }
@@ -15,21 +18,29 @@ namespace TekkenApp.Data
         Dictionary<string, string> TextMap { get; set; }
         TekkenDbContext TekkenDbContext { get; set; }
 
+
         public CommanderMapperService(TekkenDbContext _tekkenDbContext)
         {
             TekkenDbContext = _tekkenDbContext;
-            SetMaps();
+            InitMaps();
         }
 
-        private void SetMaps()
+        /// <summary>
+        /// 각 키 맵들을 초기화
+        /// </summary>
+        private void InitMaps()
         {
             StateGroupMap = new Dictionary<int, StateGroupMap>();
             StateMap = new Dictionary<int, StateMap>();
             SetKeyMaps();
             SetStateGroups();
             SetState();
-
         }
+
+
+        /// <summary>
+        /// 키보드맵 초기화
+        /// </summary>
         private void SetKeyMaps()
         {
             KeyMap = TekkenDbContext.Command.Select(p => new { p.key, p.CommandCode })
@@ -37,6 +48,9 @@ namespace TekkenApp.Data
             .ToDictionary(k => k.key, v => v.CommandCode);
         }
 
+        /// <summary>
+        /// StateGroup맵 초기화
+        /// </summary>
         private void SetStateGroups()
         {
             IList<StateGroup> StateGroupList = TekkenDbContext.StateGroup.Include(s => s.NameSet).ToList();
@@ -54,6 +68,9 @@ namespace TekkenApp.Data
             }
         }
 
+        /// <summary>
+        /// State맵 초기화
+        /// </summary>
         private void SetState()
         {
             IList<State> StateList = TekkenDbContext.State.Include(s => s.NameSet).ToList();
@@ -71,6 +88,11 @@ namespace TekkenApp.Data
             }
         }
 
+        /// <summary>
+        /// 키보드 입력값 매핑
+        /// </summary>
+        /// <param name="formedKey">입력받은 키</param>
+        /// <returns>매핑된 키 값</returns>
         public string MapKey(string formedKey)
         {
             string key = string.Empty;
@@ -82,10 +104,15 @@ namespace TekkenApp.Data
             return key;
         }
 
+        /// <summary>
+        /// state 입력값 매핑
+        /// </summary>
+        /// <param name="stateCode"></param>
+        /// <param name="languageCode"></param>
+        /// <returns>매핑된 state값</returns>
         public string MapState(string stateCode, string languageCode)
         {
             return StateMap[int.Parse(stateCode)].name[languageCode];
         }
-
     }
 }
--- TekkenApp.Models/Data/CommonServices/Interfaces/ICommanderMapperService.cs
+++ TekkenApp.Models/Data/CommonServices/Interfaces/ICommanderMapperService.cs
@@ -1,14 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TekkenApp.Data
+﻿namespace TekkenApp.Data
 {
     public interface ICommanderMapperService
     {
+        /// <summary>
+        /// 키보드 입력값 매핑
+        /// </summary>
+        /// <param name="formedKey">입력받은 키</param>
+        /// <returns>매핑된 키 값</returns>
         string MapKey(string formedKey);
+
+        /// <summary>
+        /// state 입력값 매핑
+        /// </summary>
+        /// <param name="stateCode"></param>
+        /// <param name="languageCode"></param>
+        /// <returns>매핑된 state값</returns>
         string MapState(string stateCode, string languageCode);
     }
 }
